00ac7666dc6043d19a3aff66671fa2ecbc2fae99
Jayven Nhan <nhantrivinh@gmail.com>
2022-08-16 01:02:06 -0700

    Add initial project files

--- BeginnerAR/ViewController.swift
+++ BeginnerAR/ViewController.swift
@@ -7,18 +7,49 @@
 
 import UIKit
 import RealityKit
+import ARKit
 
 class ViewController: UIViewController {
-    
-    @IBOutlet var arView: ARView!
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        
-        // Load the "Box" scene from the "Experience" Reality File
-        let boxAnchor = try! Experience.loadBox()
-        
-        // Add the box anchor to the scene
-        arView.scene.anchors.append(boxAnchor)
+  @IBOutlet var arView: ARView!
+  private let faceTrackingConfiguration: ARFaceTrackingConfiguration = {
+    let faceTrackingConfiguration = ARFaceTrackingConfiguration()
+    faceTrackingConfiguration.isLightEstimationEnabled = true
+    return faceTrackingConfiguration
+  }()
+  private let hat: Experience.Hat
+  // MARK: - Initializers
+  required init?(coder: NSCoder) {
+    guard let hat = try? Experience.loadHat() else {
+      fatalError("Unable to load completely load Experience.loadHat components.")
     }
+    self.hat = hat
+    super.init(coder: coder)
+  }
+  // MARK: - View Life Cycles
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    arView.session.delegate = self
+  }
+  override func viewDidAppear(_ animated: Bool) {
+    super.viewDidAppear(animated)
+    resetTrackingConfiguration()
+  }
+  private func resetTrackingConfiguration() {
+    arView.session.run(
+      faceTrackingConfiguration,
+      options: [.removeExistingAnchors, .resetTracking]
+    )
+  }
+}
+
+// MARK: - ARSessionDelegate
+extension ViewController: ARSessionDelegate {
+  func session(_ session: ARSession, didAdd anchors: [ARAnchor]) {
+    for case let faceAnchor as ARFaceAnchor in anchors {
+      let anchorEntity = AnchorEntity(anchor: faceAnchor)
+      anchorEntity.addChild(hat)
+      arView.scene.addAnchor(anchorEntity)
+    }
+  }
+  
 }
